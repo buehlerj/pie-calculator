19df38a6d11a3213c0a9d1504a4a88ab0f020c33
Jeffrey Buehler <jeffreydeanbuehler@gmail.com>
2020-03-22 00:46:53 -0600

    initial quick calculation form

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -6,7 +6,34 @@ import { Component } from '@angular/core';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+  pieAmeter: number;
+  price: number;
+  sliceCount: number;
 
-  constructor() {}
+  zaSpecs: any;
 
+  constructor() {
+    this.pieAmeter = 12;
+    this.price = 15;
+    this.sliceCount = 8;
+    this.calculateZaSpecs();
+  }
+
+  printPieWidth() {
+    console.log(this.pieAmeter);
+  }
+
+  calculateZaSpecs() {
+    const pieArea = Math.PI * Math.pow(this.pieAmeter / 2, 2);
+    this.zaSpecs = {
+      area: pieArea,
+      pricePerIn: this.price / pieArea,
+      inchesPerDollar: pieArea / this.price
+    };
+
+    if (this.sliceCount) {
+      this.zaSpecs.areaPerSlice = pieArea / this.sliceCount;
+      this.zaSpecs.dollarsPerSlice = this.price / this.sliceCount;
+    }
+  }
 }
